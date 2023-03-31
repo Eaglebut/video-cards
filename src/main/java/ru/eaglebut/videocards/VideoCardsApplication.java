ccc2df92d8e23f3f46dd91aaa2da83d4789e5d5b
Veniamin Tischenkov <eaglebuttt@gmail.com>
2023-03-31 12:31:09 +0300

    Добавил сервис video-cards

---
+++ src/main/java/ru/eaglebut/videocards/VideoCardsApplication.java
@@ -0,0 +1,13 @@
+package ru.eaglebut.videocards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoCardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoCardsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/eaglebut/videocards/VideoCardsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.eaglebut.videocards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoCardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
